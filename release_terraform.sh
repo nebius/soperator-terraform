d6f8497347a030745557a05415313fc8efa12c39
pavel.sofrony <pavel.sofrony@nebius.com>
2024-08-26 12:09:01 +0000

    MSP-2533 divide operator and terraform release
    X-Arc-Original-Commit: 91d34b6d052b38ad4cec59195534748bf7152559

--- release_terraform.sh
+++ release_terraform.sh
@@ -16,14 +16,14 @@ done
 
 version=$(echo "$VERSION" | tr '.' '_' | tr '-' '_')
 
-tarball="terraform/releases/unstable/slurm_operator_tf_${version}.tar.gz"
+tarball="releases/unstable/slurm_operator_tf_${version}.tar.gz"
 if [ ! -f "$tarball" ] || [ -n "$force" ]; then
     tar -czf "$tarball" \
-        --exclude="terraform/oldbius/.terraform" \
-        --exclude="terraform/oldbius/.terraform.lock.hcl" \
-        --exclude="terraform/oldbius/.terraform.tfstate.lock.info" \
-        --exclude="terraform/oldbius/terraform.tfstate" \
-        --exclude="terraform/oldbius/terraform.tfstate.backup" \
-        terraform/oldbius test
+        --exclude="oldbius/.terraform" \
+        --exclude="oldbius/.terraform.lock.hcl" \
+        --exclude="oldbius/.terraform.tfstate.lock.info" \
+        --exclude="oldbius/terraform.tfstate" \
+        --exclude="oldbius/terraform.tfstate.backup" \
+        oldbius test
     echo "Created $(pwd)/$tarball"
 fi
