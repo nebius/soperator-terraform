ee96513ab2151a14ef67349ed9e2bf8d2cd443c4
Dmitry Starov <dmitry.staroff@gmail.com>
2024-09-17 13:52:06 +0200

    Refactor newbius TF

--- newbius/modules/filestore/variables.tf
+++ newbius/modules/filestore/variables.tf
@@ -1,52 +1,14 @@
-# region iam
-
 variable "iam_project_id" {
-  description = "Project ID."
+  description = "ID of the IAM project."
   type        = string
-  nullable    = false
-}
-
-# endregion iam
-
-# region k8s
-
-variable "k8s_cluster_id" {
-  description = "ID of the k8s cluster."
-  type        = string
-  nullable    = false
 }
 
 variable "k8s_cluster_name" {
   description = "Name of the k8s cluster."
   type        = string
-  nullable    = false
 }
 
-# endregion k8s
-
-# region jail
-
-variable "jail" {
-  description = "Specification for 'jail' filestore."
-  type = object({
-    disk_type            = string
-    size_gibibytes       = number
-    block_size_kibibytes = number
-  })
-  nullable = false
-}
-
-data "units_data_size" "jail_storage" {
-  gibibytes = var.jail.size_gibibytes
-}
-
-data "units_data_size" "jail_block" {
-  kibibytes = var.jail.block_size_kibibytes
-}
-
-# endregion jail
-
-# region controller-spool
+#---
 
 variable "controller_spool" {
   description = "Specification for 'controller-spool' filestore."
@@ -58,17 +20,15 @@ variable "controller_spool" {
   nullable = false
 }
 
-data "units_data_size" "controller_spool_storage" {
-  gibibytes = var.controller_spool.size_gibibytes
+variable "jail" {
+  description = "Specification for 'jail' filestore."
+  type = object({
+    disk_type            = string
+    size_gibibytes       = number
+    block_size_kibibytes = number
+  })
+  nullable = false
 }
-
-data "units_data_size" "controller_spool_block" {
-  kibibytes = var.controller_spool.block_size_kibibytes
-}
-
-# endregion controller-spool
-
-# region jail submounts
 
 variable "jail_submounts" {
   description = "Specification for 'jail' submount filestores."
@@ -80,31 +40,3 @@ variable "jail_submounts" {
   }))
   default = []
 }
-
-data "units_data_size" "jail_submount_storage" {
-  for_each = tomap({ for submount in var.jail_submounts :
-    submount.name => submount.size_gibibytes
-  })
-
-  gibibytes = each.value
-}
-
-data "units_data_size" "jail_submount_block" {
-  for_each = tomap({ for submount in var.jail_submounts :
-    submount.name => submount.block_size_kibibytes
-  })
-
-  kibibytes = each.value
-}
-
-# endregion jail submounts
-
-# region labels
-
-variable "common_labels" {
-  description = "Labels to set for filestores."
-  type        = map(string)
-  default     = {}
-}
-
-# endregion labels
---
+++ newbius/modules/k8s/k8s_ng_gpu.tf
@@ -0,0 +1,90 @@
+locals {
+  gpu = {
+    cluster = {
+      create = tomap({
+        "8gpu-128vcpu-1600gb" = true
+        "1gpu-20vcpu-200gb"   = false
+      })[var.node_group_gpu.resource.preset]
+
+      name = join("-", [
+        trimsuffix(
+          substr(
+            var.name,
+            0,
+            64 - (length(var.node_group_gpu.gpu_cluster.infiniband_fabric) + 1)
+          ),
+          "-"
+        ),
+        var.node_group_gpu.gpu_cluster.infiniband_fabric
+      ])
+    }
+
+    count = tomap({
+      "8gpu-128vcpu-1600gb" = 8
+      "1gpu-20vcpu-200gb"   = 1
+    })[var.node_group_gpu.resource.preset]
+  }
+}
+
+resource "nebius_compute_v1_gpu_cluster" "this" {
+  count = local.gpu.cluster.create ? 1 : 0
+
+  parent_id = var.iam_project_id
+
+  name = local.gpu.cluster.name
+
+  infiniband_fabric = var.node_group_gpu.gpu_cluster.infiniband_fabric
+}
+
+resource "nebius_mk8s_v1_node_group" "gpu" {
+  depends_on = [
+    nebius_mk8s_v1_cluster.this,
+    nebius_compute_v1_gpu_cluster.this,
+  ]
+
+  parent_id = nebius_mk8s_v1_cluster.this.id
+
+  name = module.labels.name_node_group_gpu
+  labels = merge(
+    module.labels.label_group_name_gpu
+  )
+
+  version          = var.k8s_version
+  fixed_node_count = var.node_group_gpu.size
+
+  template = {
+    metadata = {
+      labels = module.labels.label_group_name_gpu
+    }
+    taints = [{
+      key    = "nvidia.com/gpu",
+      value  = local.gpu.count
+      effect = "NO_SCHEDULE"
+    }]
+
+    resources = {
+      platform = var.node_group_gpu.resource.platform
+      preset   = var.node_group_gpu.resource.preset
+    }
+    gpu_cluster = local.gpu.cluster.create ? one(nebius_compute_v1_gpu_cluster.this) : null
+
+    boot_disk = {
+      type       = var.node_group_gpu.boot_disk.type
+      size_bytes = provider::units::from_gib(var.node_group_gpu.boot_disk.size_gibibytes)
+    }
+
+    filesystems = concat([{
+      attach_mode = "READ_WRITE"
+      mount_tag   = var.filestores.jail.mount_tag
+      existing_filesystem = {
+        id = var.filestores.jail.id
+      }
+      }], [for submount in var.filestores.jail_submounts : {
+      attach_mode = "READ_WRITE"
+      mount_tag   = submount.mount_tag
+      existing_filesystem = {
+        id = submount.id
+      }
+    }])
+  }
+}
